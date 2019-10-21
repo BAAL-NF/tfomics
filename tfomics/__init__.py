346de221db4381f67b36e1ad779f2de95a381fd7
Øyvind Almelid <oyvind.almelid@igmm.ed.ac.uk>
2019-10-21 14:20:20 +0100

    add ASB to imports

--- tfomics/__init__.py
+++ tfomics/__init__.py
@@ -1,3 +1,5 @@
 """Helper functions to perform various operations on genome snippets"""
 
 from .dinucleotide_shuffle import dinucleotide_shuffle
+from .data_structures.allele_seq import AlleleSeqData
+from .data_structures.reference_genome import ReferenceGenome
